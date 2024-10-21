d9ec7c27b1c6944d58554936c875974e4fb60dd3
Anik-Howlader <anik.bioinfo@gmail.com>
2024-10-21 19:36:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const map = L.map('map', { zoomControl: false }).setView([24.723367492217395, 90
 
         if (foundLocations.length > 0) {
             foundLocations.forEach(location => {
-                map.setView([location.lat, location.lng], 20);
+                map.setView([location.lat, location.lng], 18);
                 
                 if (currentMarker) {
                     map.removeLayer(currentMarker);
